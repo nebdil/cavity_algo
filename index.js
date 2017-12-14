3dd7ad60ef39b226cb017a98673568d2f14e85b5
nebdil <dilannebioglu@gmail.com>
2017-12-13 23:13:15 -0500

    refactored

--- index.js
+++ index.js
@@ -11,38 +11,34 @@ cavity.area = function(arr) {
   // get max1 and max2
   for (let i = 0; i < arr.length; i++) {
     // find the highest block
-    if (max1 < arr[i]) {
+    if (arr[i] > max1) {
+      // when something is bigger than the current biggest, make the second biggest equal to the first one
+      max2 = max1;
+      index2 = index1;
+      // and make the current biggest equal to the newly found bigger value
       max1 = arr[i];
       index1 = i;
-    }
-  }
-  for (let i = 0; i < arr.length; i++) {
-    // find the second highest or the same height
-    if (max2 < arr[i] && i !== index1) {
+    } else if (arr[i] > max2) {
+      // if the biggest is found, check to see if there are bigger values than the second biggest
       max2 = arr[i];
       index2 = i;
     }
   }
-  // see if there are any blocks in between them
-  if(Math.abs(index1 - index2) >= 2) {
-    // find the values of the ones in between
-    if (index1 < index2) {
-      // if index1 is on the left
-      for (let i = (index1 + 1); i < index2; i++) {
-        // calculate the water
-        sumOfDiff += (max2 - arr[i]);
-      }
-    } else {
-      // if index2 is on the left
-      for (let i = (index2 + 1); i < index1; i++) {
-        // calculate the water
-        sumOfDiff += (max2 - arr[i]);
-      }
-    }
 
+  // see if there are any blocks in between the 2 highest values
+  if (Math.abs(index1 - index2) >= 2) {
+    arr.forEach((e, i) => {
+      // get the range for the values in the cavity
+      if ((index1 < i && i < index2) || (index2 < i && i < index1)) {
+        // calculate the water by getting the difference between the middle block values and the second highest value
+        sumOfDiff += (max2 - e);
+      }
+    })
     // return sum of differences
     return sumOfDiff;
+
   } else {
+    // no water found
     return 0;
   }
 }
