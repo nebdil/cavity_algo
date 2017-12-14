8ee693a1ea0a56980842828d014be8fb138a580b
nebdil <dilannebioglu@gmail.com>
2017-12-13 19:50:10 -0500

    considers if the max heights are equal to each other

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ cavity.area = function(arr) {
   let index1 = 0;
   let max2 = 0;
   let index2 = 0;
-  let diff = 0;
+  let sumOfDiff = 0;
 
   // get max1 and max2
   for (let i = 0; i < arr.length; i++) {
+    // find the highest block
     if (max1 < arr[i]) {
       max1 = arr[i];
       index1 = i;
     }
   }
   for (let i = 0; i < arr.length; i++) {
-    if (max2 < arr[i] && arr[i] < max1) {
+    // find the second highest or the same height
+    if (max2 < arr[i] && i !== index1) {
       max2 = arr[i];
       index2 = i;
     }
@@ -25,16 +27,21 @@ cavity.area = function(arr) {
   if(Math.abs(index1 - index2) >= 2) {
     // find the values of the ones in between
     if (index1 < index2) {
+      // if index1 is on the left
       for (let i = (index1 + 1); i < index2; i++) {
-        diff += (max2 - arr[i]);
+        // calculate the water
+        sumOfDiff += (max2 - arr[i]);
       }
     } else {
+      // if index2 is on the left
       for (let i = (index2 + 1); i < index1; i++) {
-        diff += (max2 - arr[i]);
+        // calculate the water
+        sumOfDiff += (max2 - arr[i]);
       }
     }
-    // get sum of differences
-    return diff;
+
+    // return sum of differences
+    return sumOfDiff;
   } else {
     return 0;
   }
--- test/test.js
+++ test/test.js
@@ -5,6 +5,9 @@ describe('area', function() {
   it('should return 3', function() {
     assert.equal(3, cavity.area([4, 6, 4, 3, 5, 2]));
   });
+  it('should return 5', function() {
+    assert.equal(5, cavity.area([4, 6, 4, 3, 6, 2]));
+  });
   it('should return 11', function() {
     assert.equal(11, cavity.area([5, 6, 7, 2, 1, 8, 5]));
   });
